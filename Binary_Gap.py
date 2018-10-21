7d7d3364e78ba53369e432d3b8e3ee529bc60390
johngriffin21 <37744760+johngriffin21@users.noreply.github.com>
2018-10-21 14:33:15 +0100

    Update Binary_Gap.py

--- Binary_Gap.py
+++ Binary_Gap.py
@@ -1,15 +1,26 @@
-def string_maker(X):
-    list_zeros = '1' + '0' * X + '1'
+#Write a function:
+
+#    def solution(N)
+
+#that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap. 
+
+
+#For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of
+#length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+
+
+def string_maker(X):                    #Create a string of zeros that we can match
+    list_zeros = '1' + '0' * X + '1'    #Needs a one before and behind it
     return list_zeros
 
 
 def solution(N):
-    binary_number = str(bin(N))[2:]  # Convert N to binary using pythons built in
+    binary_number = str(bin(N))[2:]     #Convert N to binary using pythons built in
     len_binary = len(binary_number)
 
-    for t in reversed(range(len_binary)): #doing this in reverse should increase efficiency
-        li = string_maker(t)
+    for number_of_zeros in reversed(range(len_binary)): #Doing this in reverse should increase efficiency
+        li = string_maker(number_of_zeros)
         if li in binary_number:
-            return t
-    return 0                               #no amount of zeros have been matches
+            return number_of_zeros
+    return 0                               #No amount of zeros have been matches
 
